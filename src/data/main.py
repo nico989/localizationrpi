70887ef92cab8e249cff6dad5770ce47f6be0807
Nicolo Vinci <vincinicolo98@gmail.com>
2020-03-11 20:42:54 +0100

    Add first exceptions, complete get and post method of Req class

--- src/data/main.py
+++ src/data/main.py
@@ -1,20 +1,4 @@
-from req import Req
-import requests
-import json
-from requests.auth import HTTPBasicAuth
-
 def main():
-    #r=Req()
-    #r = requests.get('http://pi:caramella98@192.168.1.69:2501/devices/views/all_views.json')
-    #print(json.loads(r.content))
-    url = 'http://pi:caramella98@192.168.1.69:2501/devices/views/phydot11_accesspoints/devices.json'
-    head = {"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}
-    j = { "devices" : "AC: 75: 1D: 57: 8A: D8" }
-    dat = "json=" + json.dumps(j)
-    print(dat)
-    x = requests.post(url, headers=head)#, data=dat)
-    print(x.status_code)
-    print('\n')
-    print(x.text)
-
-main()
+    pass
+if __name__ == "__main__":
+    main()
--- src/data/req.py
+++ src/data/req.py
@@ -1,12 +1,22 @@
-import json
-import requests
-
+import json, requests
+from exception import MyError
 class Req:
     def __init__(self, ipAddr=None):
-        self.ipAddr = ipAddr or "localhost"
+        self.__ipAddr = ipAddr or 'localhost'
+        self.__head = {"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}
+        self.__url = 'http://pi:caramella98@' + self.__ipAddr +':2501'
 
-    def get(self):
-        pass
+    def get(self, path, params):
+        response = requests.get(url=self.__url+path, params=params)
+        if(response.ok):
+            return json.loads(response.text)
+        else:
+            raise MyError('HTTP request error')
+        
 
-    def post(self):
-        pass
+    def post(self, path, data):
+        response = requests.post(url=self.__url+path, headers=self.__head, data=data)
+        if(response.ok):
+            return json.loads(response.text)
+        else:
+            raise MyError('HTTP request error')
