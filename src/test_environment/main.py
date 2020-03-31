37ca4a93f85949a1060609febd2b2c21f3349350
nicolovinci <vincinicolo98@gmail.com>
2020-03-31 21:40:23 +0200

    Check gitignore and numpy

--- src/test_environment/main.py
+++ src/test_environment/main.py
@@ -3,11 +3,12 @@ from regression import Regression
 from plot import Plot
 
 def main():
-     data = CollectData('192.168.1.69', 'AC:75:1D:57:8A:D8')
+     #data = CollectData('192.168.1.69', 'AC:75:1D:57:8A:D8')
      # collect data foreach distance
      # call AddRSSI of regression
      # call regression
      # plot line above point with datas gave from regression
+     pass
 
 if __name__=="__main__":
     main()
