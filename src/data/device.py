04acc22b3ebec030615f1e27a73d828ccb51bcc5
nicolovinci <vincinicolo98@gmail.com>
2020-04-04 19:36:51 +0200

    Add getDeviceByMAC and getClientsLastTimeSec, choose K and A values for indoor

--- src/data/device.py
+++ src/data/device.py
@@ -9,7 +9,8 @@ class Device:
                                     'kismet.device.base.channel',
                                     'kismet.device.base.frequency',
                                     'kismet.device.base.signal/kismet.common.signal.last_signal',
-                                    'kismet.device.base.type'
+                                    'kismet.device.base.type',
+                                    'kismet.device.base.last_time'
                                     ]}
         self._K = 16.114
         self._A = -46.0141
@@ -26,14 +27,33 @@ class Device:
         except:
             raise IPError('Wrong IP')
 
+    def getDeviceByMAC(self, macAddr):
+            try:
+                return self._req.post('/devices/by-mac/' + macAddr + '/devices.json', self._fields)
+            except:
+                raise IPError('Wrong IP')
+
     def getClients(self):
         try:
-            devices = self._req.post('/devices/views/phy-IEEE802.11/devices.json', self._fields)
             clients = []
+            devices = self._req.post('/devices/views/phy-IEEE802.11/devices.json', self._fields)
             for device in devices:
                 if device['kismet.device.base.type']=='Wi-Fi Client':
                     clients.append(device)
             return clients
+        except:
+            raise IPError('Wrong IP')
+
+    def getClientsLastTimeSec(self, time):
+        try:
+            clients = []
+            initialTime = self._req.get('/system/timestamp.json', None)
+            devices = self._req.post('/devices/views/phy-IEEE802.11/devices.json', self._fields)
+            for device in devices:
+                if device['kismet.device.base.type']=='Wi-Fi Client' and initialTime['kismet.system.timestamp.sec'] - device['kismet.device.base.last_time'] < time :
+                    clients.append(device)
+            return clients
+            
         except:
             raise IPError('Wrong IP')
 
@@ -51,6 +71,17 @@ class Device:
     def calcDistance(self, power):
         d = pow(10, (self._A-power)/self._K)
         return self._truncate(d, 3)
+    
+    ''' Value INDOOR
+    def indoor(self):
+        self._K = 
+        self._A = 
+    '''
+    '''Value OUTDOOR
+    def outdoor(self):
+        self._K = 
+        self._A =
+    '''
 
     def _truncate(self, n, decimals=0):
         multiplier = 10 ** decimals
